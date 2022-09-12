12e58a541c23f34ba3220d301b3ee7a0a889240d
OpeOginni <brightoginni123@gmail.com>
2022-09-12 09:14:14 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+function gitHub1() {
+  window.open("https://github.com/OpeOginni/Bulk-Transfer-BackEnd", "_blank");
+}
+
+function gitHub2() {
+  window.open(
+    "https://github.com/OpeOginni/nextjs-smartcontract-lottery-fcc",
+    "_blank"
+  );
+}
+
+function website1() {
+  window.open("https://red-bread-5267.on.fleek.co", "_blank");
+}
+
+function gitHub3() {
+  window.open("https://github.com/OpeOginni/uniswapPoolPriceBot", "_blank");
+}
+
+function linkedin() {
+  window.open(
+    "https://www.linkedin.com/in/opeyemi-oginni-9468a0232/",
+    "_blank"
+  );
+}
+
+function gitHubProfile() {
+  window.open("https://github.com/OpeOginni", "_blank");
+}
